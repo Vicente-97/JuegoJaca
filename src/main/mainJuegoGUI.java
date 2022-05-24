a54091b2d0b3c344faf7fc6d914fe08d423ab8b2
Vicente-97 <vicentevillarreal123@gmail.com>
2022-05-24 15:36:39 +0200

    Ultimo commit, arreglado errores.

--- src/main/mainJuegoGUI.java
+++ src/main/mainJuegoGUI.java
@@ -15,7 +15,7 @@ public class mainJuegoGUI {
 	public static Scanner teclado = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		try {
 			
 			////////////////////////////////////////////				
